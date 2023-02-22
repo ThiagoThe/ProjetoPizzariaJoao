8ed785d080cc5820e58623d7fb3dfcfec143d6d5
ThiagoThe <nitrox2thi@gmail.com>
2023-02-21 22:18:16 -0300

    adicionado pag pedidos, css e js relacionais

---
+++ js/pedido.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("pizza-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Gera um número aleatório único com 8 dígitos
+  const codigoPedido = Math.floor(Math.random() * 90000000) + 10000000;
+
+  // Cria um campo oculto no formulário com o código do pedido
+  const codigoPedidoInput = document.createElement("input");
+  codigoPedidoInput.setAttribute("type", "hidden");
+  codigoPedidoInput.setAttribute("name", "codigoPedido");
+  codigoPedidoInput.setAttribute("value", codigoPedido);
+
+  // Adiciona o campo oculto ao formulário
+  form.appendChild(codigoPedidoInput);
+
+  // Envia o formulário
+  form.submit();
+});
